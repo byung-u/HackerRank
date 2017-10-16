f88a6d7c8148c9734db8bb57e340fc86ab022830
byung-u <iam.byungwoo@gmail.com>
2017-10-16 16:21:56 +0900

    Fix wrong solution.

--- Algorithms/Implementation/viral_advertising.py
+++ Algorithms/Implementation/viral_advertising.py
@@ -1,16 +1,12 @@
 #!/usr/bin/env python3
-
-import sys
 from math import floor
 
 
-n = int(input().strip())
-
-m = 5
-m = floor(m/2)
-for i in range(0, n + 1):
-    m = n * floor(m / 2)
-    print(i, m)
-
-
-
+days = int(input().strip())
+n = 5
+liked = 0
+for _ in range(days):
+    like = floor(n/2)
+    n = floor(n/2) * 3
+    liked += like
+print(liked)
