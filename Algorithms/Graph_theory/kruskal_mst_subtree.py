7cff88eb3c746ddde1e543f038bc9fd528fa61c6
byung-u <iam.byungwoo@gmail.com>
2017-11-24 14:53:25 +0900

    Fix kruskal solution

--- Algorithms/Graph_theory/kruskal_mst_subtree.py
+++ Algorithms/Graph_theory/kruskal_mst_subtree.py
@@ -1,30 +1,64 @@
 #!/usr/bin/env python3
 from operator import itemgetter
 # reference
-# https://github.com/ynyeh0221/HackerRank/blob/master/Kruskal%20(MST):%20Really%20Special%20Subtree.py
+# https://inanemathgeek.wordpress.com/2012/10/15/euler-107-back-to-python/
 
+class DisjointSet (dict):
+    def add(self, item):
+        self[item] = item
 
-def Kruskal(node, edge):
-    cost = 0
-    while edge:
-        if ( (edge[0][0] not in node[edge[0][1] - 1]) or
-             (edge[0][1] not in node[edge[0][0] - 1])):
+    def find(self, item):
+        parent = self[item]
+        while self[parent] != parent:
+            parent = self[parent]
+        self[item] = parent
+        return parent
 
-            node[edge[0][0] - 1] = node[edge[0][0] - 1] | node[edge[0][1] - 1]
-            node[edge[0][1] - 1] = node[edge[0][0] - 1] | node[edge[0][1] - 1]
+    def union(self, item1, item2):
+        self[item2] = self[item1]
 
-            for i in node[edge[0][0] - 1]:
-                node[i - 1] = node[edge[0][0] - 1]
-            cost += edge[0][2]
+def kruskal(nodes, edges):
+    forest = DisjointSet()
+    mst = []
+    for n in nodes:
+        forest.add(n)
 
-        edge.pop(0)
-    print(cost)
+    sz = len(nodes) - 1
+
+    for e in sorted(edges, key=itemgetter(2)):
+        n1, n2, _ = e
+        t1 = forest.find(n1)
+        t2 = forest.find(n2)
+        if t1 != t2:
+            mst.append(e)
+            sz -= 1
+            if sz == 0:
+                return mst
+            forest.union (t1, t2)
+
+def convert2Edges (m):
+    e = []
+    for i in range (1, len(m)):
+        for j in range (0, i):  # check half of 2d array
+            if m[i][j] != 0:
+                e.append([str(i), str(j), m[i][j]])
+    return e
+
+def generateNodes (m):
+    return [str(i) for i in range(0, len(m))]
+
+def calcWeight(e):
+    w = 0
+    for i in e:
+        w += i[2]
+    return w
 
 
 t = input().split()
 N, M = int(t[0]), int(t[1])
 edge = [list(map(int, input().split())) for _ in range(M)]
 edge = sorted(edge, key=itemgetter(2))
-node = [set([i]) for i in range(1, N + 1)]
+node = [i for i in range(1, N + 1)]
 
-Kruskal(node, edge)
+e1 = kruskal(node, edge)
+print(calcWeight(e1))
