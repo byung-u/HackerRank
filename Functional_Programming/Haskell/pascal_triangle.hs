256ed571efbe8e6a8f5d3fb1ca54cac288859328
byung-u <iam.byungwoo@gmail.com>
2018-03-19 11:49:26 +0900

    Fix solution

--- Functional_Programming/Haskell/pascal_triangle.hs
+++ Functional_Programming/Haskell/pascal_triangle.hs
@@ -1,17 +1,25 @@
 module Main where
 import System.IO
 
-pas m n 
-  | n == 0 = 1
-  | m == n = 1
-  | otherwise = pas (m-1) (n-1) + pas (m-1) n
+printPascal xs n
+    | n==0      = return ()
+    | otherwise = do putStrLn [y | ys <- map show xs, y <- ys ++ " "]
+                     printPascal ([1] ++ [x+y | (x,y) <- zip xs (tail xs)] ++ [1]) (n-1)
 
-row m = map (pas m) [0..m]
-tri m = map (row) [0..(m-1)]
+main = do n <- readLn :: IO Int
+          printPascal [1] n
 
-
--- This part is related to the Input/Output and can be used as it is
--- Do not modify it
-main = do
-   m <- readLn :: IO Int
-   print(tri m)
+-- pas m n 
+--   | n == 0 = 1
+--   | m == n = 1
+--   | otherwise = pas (m-1) (n-1) + pas (m-1) n
+-- 
+-- row m = map (pas m) [0..m]
+-- tri m = map (row) [0..(m-1)]
+-- 
+-- 
+-- -- This part is related to the Input/Output and can be used as it is
+-- -- Do not modify it
+-- main = do
+--    m <- readLn :: IO Int
+--    print(tri m)
